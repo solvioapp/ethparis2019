3b95111dd6f0b48a1b795c2c639ed92dfaa20fb4
xanpj <alexander.jagaciak@yahoo.de>
2019-03-09 00:56:42 +0100

    created react app | added header

---
+++ client/solviofrontend/src/components/Main.tsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Main extends Component {
+  render() {
+    return (
+      <div id="Main">
+        <main id="Main-Container" className="inner cover text-center">
+        </main>
+        </div>
+    );
+  }
+}
+
+export default Main;
