87d1971c43ae78f166f2d034bab41994ad3ea513
Albert <github@albert.sh>
2019-03-09 22:01:02 +0100

    finish path algo

--- server/learningPath.js
+++ server/learningPath.js
@@ -18,48 +18,107 @@ function cleanObj(obj){
 }
 
 async function getTopicDeps(topic){
-	topicDeps={}
-	resources = await gun.get(db.hash(topic)).get('resources').once().then();
-	reviews = await Promise.all(gun2array(resources).map((resKey)=>
+	let topicDeps={}
+	let resources = await gun.get(topic).get('resources').once().then();
+	let reviews = await Promise.all(gun2array(resources).map((resKey)=>
 		gun.get(resKey).get('reviews').once().then()
 	));
-	reviewVals = await Promise.all(flatten(reviews).map((reviKey)=>
+	//console.log(reviews)
+	let reviewVals = await Promise.all(flatten(reviews).map((reviKey)=>
 		gun.get(reviKey).once().then()
 	));
-	resourceVals = await Promise.all(gun2array(resources).map((resKey)=>
+	//console.log(reviewVals)
+	let resourceVals = await Promise.all(gun2array(resources).map((resKey)=>
 		gun.get(resKey).once().then()
 	));
-	resourceVals=_.clone(resourceVals).map(cleanObj);
-	offset=0;
+	let offset=0;
+	let finalResources=[]
 	for(i=0; i<resourceVals.length; i++){
-		if(reviews[i]==undefined){
-			resourceVals[i].reviews=null;
-			offset+=1;
-		} else{
-			resourceVals[i].reviews=cleanObj(reviewVals[i-offset]);
+		finalResources.push(cleanObj(_.clone(resourceVals[i])))
+		finalResources[i].reviews={length:0, quality:0};
+		if(reviews[i]!=undefined){
+			//console.log(gun2array(reviews[i]))
+			for(j=0; j<gun2array(reviews[i]).length; j++){
+				//console.log(reviewVals[j+offset])
+				//finalResources[i].reviews.push(cleanObj(_.clone(reviewVals[j+offset])));
+				finalResources[i].reviews={
+					length: finalResources[i].reviews.length+reviewVals[j+offset].length,
+					quality: finalResources[i].reviews.quality+reviewVals[j+offset].quality
+				};
+
+			}
+			offset+=reviews[i].length;
 		}
 	}
 
-	console.log('a')
-
-	dependencies = await Promise.all(flatten(reviews).map((revKey)=>
+	let dependencies = await Promise.all(flatten(reviews).map((revKey)=>
 		gun.get(revKey).get('dependencies').once().then()
 	));
-	console.log('a')
-	depVals = await Promise.all(flatten(dependencies).map((depKey)=>
+	let depVals = await Promise.all(flatten(dependencies).map((depKey)=>
 		gun.get(depKey).once().then()
 	));
-	console.log('a')
+	//console.log(depVals)
 	depVals.forEach((dep)=>{
 		topicDeps[dep.topic['#']]=topicDeps[dep.topic['#']]+dep.weight||dep.weight;
 	});
-	console.log(topicDeps)
-	//return {topicDeps, resourceVals};
+	//console.log('a')
+	return {topicDeps, finalResources};
 }
 
-async function getLearningPath(topic){
-	return 1
+
+
+async function getLearningPath(initialTopic, time){
+	let topics=[{
+		topic:initialTopic,
+		weight:1
+	}]
+	let resources=[]
+	while(time>=0 || topics.length==0){
+		let topic=topics[0].topic;
+		//console.log(resources)
+		let stuff=await getTopicDeps(topic)
+		let sumWeights=0;
+		Object.keys(stuff.topicDeps).forEach((t)=>sumWeights+=stuff.topicDeps[t])
+		Object.keys(stuff.topicDeps).forEach((t)=>{
+			let found=false
+			let topu={
+				topic:t,
+				weight:stuff.topicDeps[t]
+			}
+			topics.forEach((to)=>{
+				if(to.topic==topu.topic){
+					to.weight+=topu.weight/sumWeights;
+					found=true;
+				}
+			})
+			if(!found){
+				topics.push({
+					topic:topu.topic,
+					weight:topu.weight/sumWeights
+				})
+			}
+		})
+		//console.log(stuff.finalResources)
+		let maxim={}
+		let maxVal=0
+		stuff.finalResources.forEach((v)=>{
+			//console.log(v)
+			let val=Number(v.reviews.quality)*Number(v.reviews.length)
+			//console.log(val)
+			if(val > maxVal){
+				val=maxVal;
+				maxim=v
+			}
+		})
+
+		resources.push(maxim);
+		time-=maxim.reviews.length;
+		topics=topics.slice(1).sort((e1, e2)=>e1.weight<e2.weight);
+		//console.log(topics)
+	}
+	//console.log(resources);
+	return resources;
 }
 
-//getLearningPath('cryptocurrencies');
-getTopicDeps('cryptocurrencies');
+getLearningPath(db.hash('cryptocurrencies'), 70);
+//getTopicDeps('cryptocurrencies');
