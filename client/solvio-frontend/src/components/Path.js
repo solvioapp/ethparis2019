a635391dcbcd4e95770deff0fdcab930557ee7d7
dteiml <dteiml@gmail.com>
2019-03-09 19:05:27 +0100

    add paths ui

--- client/solvio-frontend/src/components/Path.js
+++ client/solvio-frontend/src/components/Path.js
@@ -2,14 +2,24 @@ import React, { Component } from 'react';
 
 import { Resource } from './Resource'
 
+import '../styles/Resource.scss'
+
 export class Path extends Component {
     render() {
         const { path } = this.props
         return (
-            <div>
+            <div className="flex">
+                <div>
+                    <button className="button" onClick=""> <strong> &lt; </strong> </button>
+                </div>
+                <div>
                 {path.map(resource => (
                     <Resource data={resource} />
                 ))}
+                </div>
+                <div>
+                    <button className="button"> <strong> &gt; </strong> </button>
+                </div>
             </div>
         );
     }
