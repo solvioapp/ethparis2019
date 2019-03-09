f52c503c242655453d10313c1f7784747976adc1
dteiml <dteiml@gmail.com>
2019-03-09 17:23:42 +0100

    add resource list skeleton

--- client/solvio-frontend/src/App.js
+++ client/solvio-frontend/src/App.js
@@ -16,15 +16,50 @@ import { Topic } from './components/Topic'
 // Css
 import './styles/styles.scss'
 
+const r1 = {
+	name: "K Tutorial",
+	topic: "K",
+	url: "https://github.com/kframework/k/tree/master/k-distribution/tutorial/",
+	duration: "500",
+	quality: "82",
+}
+
+const r2 = {
+	name: "Formal Verification Workshop 1",
+	topic: "K-EVM",
+	url: "https://www.youtube.com/watch?v=d6qHxDIeFw0",
+	duration: "75",
+	quality: "78"
+}
+
+const r3 = {
+	name: "Formal Verification Workshop 2",
+	topic: "Formal verification of smart contracts using K",
+	url: "https://www.youtube.com/watch?v=n6AgBIkHlhg",
+	duration: "75",
+	quality: "88"
+}
+
+const defaultPaths = [[r1, r2, r3], [r3, r1], [r3, r3, r2, r1]]
+
 class App extends React.Component {
 
 	constructor(props){
 		super(props)
+
+		this.state = {
+			learningPaths: defaultPaths,
+			pathIndex: 0
+		}
 	}
 
 	updateQuery(query){
 		console.log(query)
 
+	}
+
+	updatePathIndex(pathIndex) {
+		this.setState({pathIndex})
 	}
 
 	// Render the main application element
@@ -44,8 +79,11 @@ class App extends React.Component {
 						<Route path="/resource/:cid/addReview" component={AddReview} />
 						<Route path="/resource/:cid/reviews" component={Reviews} />
 						<Route path="/topic/:cid" component={Topic} />
-						<Route path="/path" render={(learningPaths) => (
-							<Path {...learningPaths} />
+						<Route path="/paths" render={() => (
+							<Path
+								path={this.state.learningPaths[this.state.pathIndex]}
+								updatePathIndex={this.updatePathIndex}
+							/>
 						)} />
 					</div>
 				</Router>
--- client/solvio-frontend/src/components/Path.js
+++ client/solvio-frontend/src/components/Path.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 
+import { Resource } from './Resource'
+
 export class Path extends Component {
     render() {
+        const { path } = this.props
         return (
-            <div></div>
-            
+            <div>
+                {path.map(resource => (
+                    <Resource data={resource} />
+                ))}
+            </div>
         );
     }
 }
