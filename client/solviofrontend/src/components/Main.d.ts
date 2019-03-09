72262009956d6cc3cec31bd94340f99c4490e187
xanpj <alexander.jagaciak@yahoo.de>
2019-03-09 14:26:52 +0100

    search bar design

--- client/solviofrontend/src/components/Main.d.ts
+++
@@ -1,3 +0,0 @@
-declare global {
-    interface window { Treant: any; }
-}
--- client/solviofrontend/src/components/Search.tsx
+++ client/solviofrontend/src/components/Search.tsx
@@ -4,69 +4,39 @@ import ReactDOM from 'react-dom'
 // Css
 import './Search.css'
 
-export class Search extends React.Component {
-    state = {
+export class Search extends React.Component<any,any> {
+  constructor(props: any){
+    super(props)
+
+    this.state = {
         query: '',
     }
+  }
 
-    handleChange = (query: any) => {
-        this.setState({
+
+  handleChange = (e: any) => {
+    e.preventDefault()
+    const query = e.target.value
+    this.setState({
             query
-        })
+      })
     }
 
     render() {
         return (
+          <div id="Search">
             <form>
-                <input
-                    placeholder="Search for..."
-                    value={this.state.query}
-                    onChange={this.handleChange}
-                />
+                <div className="search_wrapper">
+                  <input className="input"
+                      id="SearchBox"
+                      placeholder="Search"
+                      value={this.state.query}
+                      onChange={(e) => this.handleChange(e)} />
+                  <span className="underline"></span>
+                </div>
                 <p>{this.state.query}</p>
             </form>
+          </div>
         )
     }
 }
-
-// class Search extends React.Component {
-//     render() {
-//         return (
-
-//             <div>
-//                 <input type="text" placeholder="" />
-//             </div>
-//         //     <div id="DIV_1">
-//         //         <div id="DIV_2">
-//         //             <form action="/s/all" method="get" id="FORM_3">
-//         //                 <div id="DIV_4">
-//         //                     <div id="DIV_5">
-//         //                         <label 
-//         //                         // for="Koan-via-HeaderController__input" 
-//         //                         id="LABEL_6">
-//         //                             <span id="SPAN_7">Search</span>
-//         //                         </label>
-//         //                         <div id="DIV_8">
-//         //                             <svg id="svg_9">
-//         //                                 <path id="path_10">
-//         //                                 </path>
-//         //                             </svg>
-//         //                         </div>
-//         //                         <div id="DIV_11">
-//         //                             <div id="DIV_12">
-//         //                                 <input type="text" id="INPUT_13" name="query" placeholder="Try “Costa Brava”" />
-//         //                             </div>
-//         //                         </div>
-//         //                     </div>
-//         //                 </div>
-//         //             </form>
-//         //         </div>
-//         //         <div id="DIV_14">
-//         //             <button type="button" id="BUTTON_15">
-//         //                 Cancel
-// 		// </button>
-//         //         </div>
-//         //     </div>
-//         )
-//     }
-// }
