447924944101ecd3266ed5231a5c4dfad1f52dd5
dteiml <dteiml@gmail.com>
2019-03-10 01:29:51 +0100

    finish ReviewController.sol

--- client/solvio-frontend/contracts/ReviewController.sol
+++ client/solvio-frontend/contracts/ReviewController.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.5.0;
 contract ReviewController {
 
     // Deposit amount is 0.02 ETH (2.7 usd) = 2 * 10^16
+    // Should be below uint(-1)/3 for overflow not to happen
     uint constant amount = 20000000000000000;
 
     // Challenge period is 3 days
@@ -14,17 +15,17 @@ contract ReviewController {
     // mapping (identifier => Review)
     mapping (bytes32 => Review) public reviews;
     // mapping (identifier => disputer)
-    mapping (bytes32 => address) public disputes;
+    mapping (bytes32 => address payable) public disputes;
 
     struct Review {
         uint timeAdded;
-        address user;
+        address payable submitter;
     }
 
-    event NewSubmission(bytes32 identifier, address user);
+    event NewSubmission(bytes32 identifier, address submitter);
     event NewChallenge(bytes32 identifier, address disputer);
-    event NewWithdrawal(bytes32 identifier, address user);
-    // event 
+    event NewWithdrawal(bytes32 identifier, address submitter);
+    event NewResolution(bytes32 identifier, bool valid);
 
     constructor (address schelling) public {
         // Assign arbitrator
@@ -89,7 +90,7 @@ contract ReviewController {
         emit NewWithdrawal(identifier, msg.sender);
     }
 
-    function resolveDispute(bytes32 identifier) public
+    function resolveDispute(bytes32 identifier, bool valid) public
     {
         // Can only be called by arbitrator
         require(msg.sender == arbitrator, "not arbitrator");
@@ -97,16 +98,26 @@ contract ReviewController {
         // Review has to be in dispute
         require(disputes[identifier] != address(0), "Review not in dispute");
 
+        if (valid) {
+            // Pay arbitrator 1/2 of deposit
+            msg.sender.transfer(amount / 2);
 
-        msg.sender.transfer(amount / 2);
+            // Pay submitter 3/2 of deposit
+            reviews[identifier].submitter.transfer(amount * 3 / 2);
+        } else {
+            // Pay arbitrator 1/2 of deposit
+            msg.sender.transfer(amount / 2);
+            
+            // Pay challenger 3/2 of deposit
+            disputes[identifier].transfer(amount * 3 / 2);
+        }
+
+        // Null dispute
+        disputes[identifier] = address(0);
+
+        // Null review
+        reviews[identifier] = Review(0, address(0));
+
+        emit NewResolution(identifier, valid);
     }
-
-    function withdrawAfterDispute() public
-    {
-
-    }
-
-    // function withdrawMultiple(uint[] identifiers memory) public
-
-    
 }
