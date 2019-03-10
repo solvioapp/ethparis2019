cee1b786bc4ca8a94b9c15995f9ba4c9d18b3d9e
dteiml <dteiml@gmail.com>
2019-03-10 05:34:24 +0100

    add /about

--- client/solvio-frontend/src/components/MoreInfo.js
+++ client/solvio-frontend/src/components/MoreInfo.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 
+import '../styles/styles.scss'
+
 export class MoreInfo extends Component {
 
     render() {
         return (
-            <div>
+            <div className="about">
                 <h2>More info about Solvio...</h2>
-                <p>Solvio will be a decentralized education system.</p>
+                <p>Solvio will be a decentralized education system. We're building technology to enable students to engage in any form of learning that they choose and get credentials for it. We host regular calls as well as a <a href="https://solvio.discourse.group" className="link">Discourse</a>. Join us!</p>
             </div>
         );
     }
